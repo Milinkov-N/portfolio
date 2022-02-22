faa768467c6d5957950c0e4d4233efc6f78d0101
Milinkov-N <milinkov.nik@gmail.com>
2022-02-22 14:30:49 +0400

    added Logo component

---
+++ components/Logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <div className='logo'>
+      <span className='primary-clr'>{ '<' }</span>
+      <span>{ 'Milinkoff ' }</span>
+      <span className='primary-clr'>{ '/>' }</span>
+    </div>
+  )
+}
