7c94305b11629a64a2ebe019410d11e3bde3fb48
Milinkov-N <milinkov.nik@gmail.com>
2022-02-23 13:35:28 +0400

    created Heading component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,4 +1,5 @@
 import TagWrapper from './TagWrapper'
+import { Heading } from './typography'
 import Wrapper from './Wrapper'
 
 export default function Hero() {
@@ -6,8 +7,14 @@ export default function Hero() {
     <section className='hero'>
       <Wrapper>
         <TagWrapper tag='Web-Developer'>
-          <h1>Никита Милиньков</h1>
-          <p>Специализируюсь на создании UI/UX, разработке адаптивных и современных вебсайтов</p>
+          <Heading
+            as='h1'
+            level={ 1 }
+            label='Никита Милиньков'
+          />
+          <p>
+            Специализируюсь на создании UI/UX, разработке адаптивных и современных вебсайтов
+          </p>
         </TagWrapper>
       </Wrapper>
     </section>
