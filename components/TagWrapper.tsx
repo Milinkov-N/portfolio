0bdff4202a2272ee43d827a02ca70a9dbc43b357
Milinkov-N <milinkov.nik@gmail.com>
2022-02-23 13:05:39 +0400

    created TagWrapper component and tweaked global styles

---
+++ components/TagWrapper.tsx
@@ -0,0 +1,25 @@
+import { ReactNode } from 'react'
+import styles from 'styles/TagWrapper.module.css'
+
+export interface ITagWrapper {
+  className?: string
+  tag: string
+  children?: ReactNode
+}
+
+export default function TagWrapper({ className, tag, children }: ITagWrapper) {
+  return (
+    <div className={ `tag-wrapper ${ className ? className : '' }` }>
+      <div className={ `${ styles.tag } opening-tag` }>
+        { `<${ tag }>` }
+      </div>
+        <div className={ styles.content }>
+          { children }
+        </div>
+      <div className={ `${ styles.tag } closing-tag` }>
+        { `</${ tag }>` }
+      </div>
+    </div>
+    
+  )
+}
